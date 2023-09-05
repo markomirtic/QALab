3de65c5df27feefb09ec3727688fb0ae4b551491
Marko Mirtic <mmirtic@apple.com>
2023-09-05 14:48:03 +0200

    LoginTestAdded

--- src/test/java/testCases/LoginTest.java
+++ src/test/java/testCases/LoginTest.java
@@ -1,22 +1,23 @@
 package testCases;
 
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
+import java.nio.channels.NotYetBoundException;
+
 public class LoginTest {
     @Test
     public void firsTest() {
         WebDriverManager.chromedriver().setup();
-       ChromeOptions options = new ChromeOptions();
-       options.addArguments("--headless");
-        WebDriver driver = new ChromeDriver();
+         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
         driver.quit();
-    }
 
+    }
 
 }
